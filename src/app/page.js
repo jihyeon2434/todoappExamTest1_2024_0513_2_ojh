52e48f0b7f2efeb7e690f0547f8858c0e423102b
jihyeon2434 <ubis970420@gmail.com>
2024-04-01 16:43:54 +0900

    button 에 tailwind와 mui 적용

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
-import Image from "next/image";
+'use client';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
-  
+    <>
+      <div>
+        <Button className="bg-red-500" variant="text">
+          Text
+        </Button>
+      </div>
+    </>
   );
 }
